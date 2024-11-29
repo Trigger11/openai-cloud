7f1541788444cb509bc56267c930f75404bfc106
Trigger11 <842913592@qq.com>
2024-11-29 10:50:42 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,18 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    const actualUrlStr =
+      url.pathname.replace("/proxy/", "") + url.search + url.hash;
+    const actualUrl = new URL(actualUrlStr);
+    const modifiedRequest = new Request(actualUrl, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: "follow",
+    });
+    const response = await fetch(modifiedRequest);
+    const modifiedResponse = new Response(response.body, response);
+    modifiedResponse.headers.set("Access-Control-Allow-Origin", "*");
+    return fetch(modifiedResponse);
+  },
+};
